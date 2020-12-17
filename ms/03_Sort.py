0edcb5478792257b6ce4ac595aee5a2c1bb1566d
MK-Mac-073 <mk-mac-073@MK-Mac-073ui-MacBookPro.local>
2020-12-18 01:23:21 +0900

    fix : minsiklee - Sort Q2

--- ms/03_Sort.py
+++ ms/03_Sort.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[40]:
 
 
 # code description
@@ -20,19 +20,57 @@ def solution(array, commands):
     return result
 
 
-# In[ ]:
+# In[43]:
 
 
-def main():
+def main_1():
     array = [1, 5, 2, 6, 3, 7, 4]
     commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 
     print(solution(array, commands))     # return [5, 6, 3]
 
 
-# In[ ]:
+# In[44]:
 
 
 if __name__ == "__main__":
-    main()
+    main_1()
+
+
+# In[70]:
+
+
+# code description
+# Author : minsik.lee
+# FileName : 03_Sort.py
+# Note 
+	# @Summary : Sort 02. 가장 큰 수
+	# @version 1.0 : (2020-12-17) 
+    # @version 2.0 : (2020-12-18) case 0, 0, 0, 0
+
+def solution(numbers):
+    answer = sorted(list(map(str, numbers)), key=lambda number_i:number_i*3, reverse=True)
+    # # [(str(number_i) * 3) for number_i in numbers] # ['333', '303', '343', '555', '999']
+
+    # answer = ''.join(answer)
+    answer = str(int(''.join(answer)))
+    return answer
+
+
+# In[67]:
+
+
+def main_2():
+    # numbers = [3, 30, 34, 5, 9]
+    numbers = [0, 0, 0, 0]
+
+
+    print(solution(numbers))     # return [5, 6, 3]
+
+
+# In[68]:
+
+
+if __name__ == "__main__":
+    main_2()
 
